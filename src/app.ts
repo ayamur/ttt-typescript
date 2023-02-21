8271b0c2010de580f9b354f2d6115a5d40ea6456
AyaMu <ayalamuruch@gmail.com>
2023-02-21 14:53:45 -0500

    function init to typescript

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 /*-------------------------------- Constants --------------------------------*/
+
 //? number[][] indicated that it's an array of arrays, where the inner arrays contain numbers.
 
 const winningCombos: number[][] = [
@@ -10,18 +11,36 @@ const winningCombos: number[][] = [
   [2, 5, 8],
   [0, 4, 8],
   [2, 4, 6]
-]
+];
 
 /*---------------------------- Variables (state) ----------------------------*/
 
+let board: number[];
+let turn: number;
+let tie: boolean;
+let winner: boolean;
 
 
 /*------------------------ Cached Element References ------------------------*/
 
+//! I am unsure on this, research further
 
+//const squareEls = document.querySelectorAll('.sqr')
+//const messageEl = document.getElementById('message')
+//const resetBtnEl = document.querySelector('button')
 
 /*----------------------------- Event Listeners -----------------------------*/
 
+//! I am unsure on this, research further
 
+//document.querySelector('.board').addEventListener('click', handleClick)
+//resetBtnEl.addEventListener('click', init)
 
 /*-------------------------------- Functions --------------------------------*/
+
+function init(): void {
+  board = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+  turn = 1;
+  winner = false;
+  tie = false;
+}
